72f2d9fb4feb9579b10a505a75585efc3722b6ee
Lars Nyström <nystrom.lasse@gmail.com>
2018-12-02 20:50:23 +0100

    Prepare for day 3

---
+++ day03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt")
+                .Where(l => !string.IsNullOrWhiteSpace(l))
+                .ToList();
+
+            
+        }
+    }
+}
---
+++ tests/day03tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace day03tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
