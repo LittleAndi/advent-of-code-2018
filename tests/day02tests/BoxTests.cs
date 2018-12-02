c7c8e33cb81a29235b9c5da81c38a2d30077613d
Lars Nyström <nystrom.lasse@gmail.com>
2018-12-02 13:41:32 +0100

    Add tests for part 2

--- tests/day02tests/BoxTests.cs
+++ tests/day02tests/BoxTests.cs
@@ -10,8 +10,7 @@ namespace day02tests
         [Fact]
         public void HasNoRepeatingLetters()
         {
-            var box = new Box();
-            box.ID = "abcdef";
+            var box = new Box { ID = "abcdef" };
             Assert.False(box.HasExactlyTwoOfAnyLetter);
             Assert.False(box.HasExactlyThreeOfAnyLetter);
         }
@@ -19,8 +18,7 @@ namespace day02tests
         [Fact]
         public void HasBothRepeatingLetters()
         {
-            var box = new Box();
-            box.ID = "bababc";
+            var box = new Box { ID = "bababc" };
             Assert.True(box.HasExactlyTwoOfAnyLetter);
             Assert.True(box.HasExactlyThreeOfAnyLetter);
         }
@@ -28,8 +26,7 @@ namespace day02tests
         [Fact]
         public void HasTwoOfAnyLetterButNotThree()
         {
-            var box = new Box();
-            box.ID = "abbcde";
+            var box = new Box { ID = "abbcde" };
             Assert.True(box.HasExactlyTwoOfAnyLetter);
             Assert.False(box.HasExactlyThreeOfAnyLetter);
         }
@@ -37,8 +34,7 @@ namespace day02tests
         [Fact]
         public void HasThreeOfAnyLetterButNotTwo()
         {
-            var box = new Box();
-            box.ID = "abcccd";
+            var box = new Box { ID = "abcccd" };
             Assert.False(box.HasExactlyTwoOfAnyLetter);
             Assert.True(box.HasExactlyThreeOfAnyLetter);
         }
@@ -46,8 +42,7 @@ namespace day02tests
         [Fact]
         public void HasDoubleTwosButNoThrees()
         {
-            var box = new Box();
-            box.ID = "aabcdd";
+            var box = new Box { ID = "aabcdd" };
             Assert.True(box.HasExactlyTwoOfAnyLetter);
             Assert.False(box.HasExactlyThreeOfAnyLetter);
         }
@@ -55,11 +50,24 @@ namespace day02tests
         [Fact]
         public void HasDoubleThreesButNoTwos()
         {
-            var box = new Box();
-            box.ID = "ababab";
+            var box = new Box { ID = "ababab" };
             Assert.False(box.HasExactlyTwoOfAnyLetter);
             Assert.True(box.HasExactlyThreeOfAnyLetter);
         }
         
+        [Fact]
+        public void HasNotExactlyOneDifferingCharacter()
+        {
+            var box = new Box { ID = "abcde" };
+            Assert.False(box.HasExactlyOneDifferingCharacter("axcye"));
+        }
+
+        [Fact]
+        public void HasExactlyOneDifferingCharacter()
+        {
+            var box = new Box { ID = "fghij" };
+            Assert.True(box.HasExactlyOneDifferingCharacter("fguij"));
+        }
+
     }
 }
