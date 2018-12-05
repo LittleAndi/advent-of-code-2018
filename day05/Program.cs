a1b10997a10f80e79ea9615631dec870a731a6a8
Lars Nyström <nystrom.lasse@gmail.com>
2018-12-05 06:40:45 +0100

    Implement Polymer tests

--- day05/Program.cs
+++ day05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace day05
 {
@@ -7,6 +8,36 @@ namespace day05
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Polymer
+    {
+        public string PolymerSequence { get; set; }
+
+        public string React()
+        {
+            var result = PolymerSequence;
+            string previousResult ="";
+            while (result != previousResult)
+            {
+                previousResult = result;
+                result = runReactionSequence(result);
+            }
+            return result;
+        }
+
+
+        private string runReactionSequence(string input)
+        {
+            for (int i = 65; i <= 90; i++)
+            {
+                var c = (char)i;
+                var replacement1 = c.ToString().ToLower() + c.ToString().ToUpper();
+                var replacement2 = c.ToString().ToUpper() + c.ToString().ToLower();
+                input = input.Replace(replacement1, "").Replace(replacement2, "");
+            }
+            return input;
         }
     }
 }
---
+++ tests/day05tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using day05;
+
+namespace day05tests
+{
+    public class UnitTest1
+    {
+        Polymer polymer = new Polymer { PolymerSequence = "dabAcCaCBAcCcaDA" };
+
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("dabCBAcaDA", polymer.React());
+        }
+    }
+}
