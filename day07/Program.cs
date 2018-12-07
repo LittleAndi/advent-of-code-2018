00b535e7d49b6ad4faf8d29608c3340edc894b8a
Lars Nyström <nystrom.lasse@gmail.com>
2018-12-07 22:02:20 +0100

    Add base project and read input

---
+++ day07/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input_mini.txt")
+            .Where(l => !string.IsNullOrWhiteSpace(l))
+            .Select(l => new Step() { From = l.Substring(5, 1)[0], To = l.Substring(36, 1) });
+
+            foreach (var item in lines.OrderBy(l => l.From))
+            {
+                System.Console.WriteLine($"{item.From}=>{item.To}");
+            }
+        }
+    }
+
+    public class Step
+    {
+        public Step()
+        {
+            Done = false;
+        }
+        public char From { get; set; }
+        public char To { get; set; }
+        public bool Done { get; set; }
+    }
+}
