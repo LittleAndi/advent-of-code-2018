4020c02c5b4bfc09d9e5503f974864f0625418b6
Lars Nyström <nystrom.lasse@gmail.com>
2018-12-11 22:51:40 +0100

    Add project for day 12

---
+++ day12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var stars = File.ReadAllLines("input.txt")
+            //.Where(l => !string.IsNullOrWhiteSpace(l))
+            //.Select((l, i) => new Star() { Input = l, ID = i })
+            //.ToList();
+
+        }
+    }
+}
