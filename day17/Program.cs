e3a76d84432920b0556248ce232b36bb32997048
Lars Nyström <nystrom.lasse@gmail.com>
2018-12-17 00:15:22 +0100

    Add empty project

---
+++ day17/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("")
+                .Where(l => !string.IsNullOrWhiteSpace(l))
+                .ToList();
+
+        }
+    }
+}
