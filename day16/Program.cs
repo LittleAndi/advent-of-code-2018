8e5235b9b1009e2592b49aae4b0d7a4e7fa0976a
Lars Nyström <nystrom.lasse@gmail.com>
2018-12-16 17:48:04 +0100

    Add solutions for day 16

--- day16/Program.cs
+++ day16/Program.cs
@@ -2,14 +2,133 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace day16
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Ops.testsample(new int[] { 3, 2, 1, 1 }, 2, 1, 2, new int[] { 3, 2, 2, 1 });
+            var lines = File.ReadAllLines("input.txt")
+                .Where(l => !string.IsNullOrWhiteSpace(l))
+                .ToList();
+
+            LinkedList<string> testInputs = new LinkedList<string>(lines);
+
+            List<Test> pgm = new List<Test>();
+            List<Test> tests = ParseInput(testInputs, out pgm);
+            Dictionary<string, int[]> mapOps = new Dictionary<string, int[]>();
+
+            int moreThanOrEqualToThree = 0;
+            Dictionary<int, string> opCodes = new Dictionary<int, string>();
+            foreach (var test in tests)
+            {
+                var result = Ops.testsample(test.Before, test.A, test.B, test.C, test.After);
+                if (result.Count >= 3) moreThanOrEqualToThree++;
+                if (result.Count == 1)
+                {
+                    if (!opCodes.ContainsKey(test.Operation))
+                    {
+                        opCodes.Add(test.Operation, result.Keys.First());
+                        Console.WriteLine($"Opcode {test.Operation} is {result.Keys.First()}");
+                    }
+                }
+                foreach (var item in result)
+                {
+                    if (!mapOps.ContainsKey(item.Key))
+                    {
+                        mapOps.Add(item.Key, new int[16]);
+                    }
+                    mapOps[item.Key][test.Operation] = 1;
+                }
+            }
+
+            Console.WriteLine(moreThanOrEqualToThree);
+
+            PrintOps(mapOps);
+
+            while (mapOps.Count >0)
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    var s = mapOps.Sum(m => m.Value[i]);
+                    if (s == 1)
+                    {
+                        var o = mapOps.Where(m => m.Value[i] == 1).First();
+                        Console.WriteLine($"{o.Key}, {i}");
+                        mapOps.Remove(o.Key);
+                    }
+                }
+            }
+
+            int[] register = new int[] { 0, 0, 0, 0 };
+            foreach (var item in pgm)
+            {
+                register = Ops.execute(item.Operation, register, item.A, item.B, item.C);
+                Console.WriteLine($"{item.Operation,2} {item.A} {item.B} {item.C} => {register[0],4}{register[1],4}{register[2],4}{register[3],4}");
+            }
+
+            Console.WriteLine(register[0]);
+
+            //Ops.testsample(new int[] { 3, 2, 1, 1 }, 2, 1, 2, new int[] { 3, 2, 2, 1 });
+        }
+
+        private static void PrintOps(Dictionary<string, int[]> mapOps)
+        {
+            foreach (var item in mapOps.OrderBy(m=>m.Key))
+            {
+                StringBuilder output = new StringBuilder();
+                for (int i = 0; i < 16; i++)
+                {
+                    output.Append($"{item.Value[i],3}");
+                }
+                Console.WriteLine($"{item.Key}\t{output.ToString()}");
+            }
+        }
+
+        private static List<Test> ParseInput(LinkedList<string> testInputs, out List<Test> pgm)
+        {
+            pgm = new List<Test>();
+            List<Test> tests = new List<Test>();
+            var testInput = testInputs.First;
+
+            Test test;
+            while (testInput != null)
+            {
+                if (testInput.Value.StartsWith("Before: "))
+                {
+                    test = new Test();
+                    test.Before = new int[] { int.Parse(testInput.Value.Substring(9, 1)), int.Parse(testInput.Value.Substring(12, 1)), int.Parse(testInput.Value.Substring(15, 1)), int.Parse(testInput.Value.Substring(18, 1)) };
+
+                    testInput = testInput.Next;
+                    var command = testInput.Value.Split(' ');
+                    test.Operation = int.Parse(command[0]);
+                    test.A = int.Parse(command[1]);
+                    test.B = int.Parse(command[2]);
+                    test.C = int.Parse(command[3]);
+                    
+                    testInput = testInput.Next;
+                    test.After = new int[] { int.Parse(testInput.Value.Substring(9, 1)), int.Parse(testInput.Value.Substring(12, 1)), int.Parse(testInput.Value.Substring(15, 1)), int.Parse(testInput.Value.Substring(18, 1)) };
+                    tests.Add(test);
+
+                    testInput = testInput.Next;
+                }
+                else
+                {
+                    var command = testInput.Value.Split(' ');
+                    var pgmOp = new Test();
+                    pgmOp.Operation = int.Parse(command[0]);
+                    pgmOp.A = int.Parse(command[1]);
+                    pgmOp.B = int.Parse(command[2]);
+                    pgmOp.C = int.Parse(command[3]);
+                    pgm.Add(pgmOp);
+                    testInput = testInput.Next;
+                }
+            }
+
+            //Console.WriteLine(tests.Count);
+            return tests;
         }
     }
 
@@ -20,39 +139,39 @@ namespace day16
             switch (operation)
             {
                 case 0:
-                    break;
+                    return bori(register, A, B, C);
                 case 1:
-                    break;
+                    return borr(register, A, B, C);
                 case 2:
-                    break;
+                    return addi(register, A, B, C);
                 case 3:
-                    break;
+                    return muli(register, A, B, C);
                 case 4:
-                    break;
+                    return addr(register, A, B, C);
                 case 5:
-                    break;
+                    return bani(register, A, B, C);
                 case 6:
-                    break;
+                    return gtri(register, A, B, C);
                 case 7:
-                    break;
+                    return setr(register, A, B, C);
                 case 8:
-                    break;
+                    return gtrr(register, A, B, C);
                 case 9:
-                    break;
+                    return seti(register, A, B, C);
                 case 10:
-                    break;
+                    return eqir(register, A, B, C);
                 case 11:
-                    break;
+                    return eqrr(register, A, B, C);
                 case 12:
-                    break;
+                    return mulr(register, A, B, C);
                 case 13:
-                    break;
+                    return eqri(register, A, B, C);
                 case 14:
-                    break;
+                    return gtir(register, A, B, C);
                 case 15:
-                    break;
+                    return banr(register, A, B, C);
             }
-            return register;
+            return register.ToArray();
         }
 
         /// <summary>
@@ -231,7 +350,7 @@ namespace day16
         public static int[] gtri(int[] register, int A, int B, int C)
         {
             var reg = register.ToArray();
-            reg[C] = (register[A] > B ? 1: 0);
+            reg[C] = (register[A] > B ? 1 : 0);
             return reg;
         }
 
@@ -295,31 +414,43 @@ namespace day16
             return reg;
         }
 
-        public static int testsample(int[] register, int A, int B, int C, int[] compare)
+        public static Dictionary<string, int> testsample(int[] register, int A, int B, int C, int[] compare)
         {
-            int equals = 0;
+            Dictionary<string, int[]> results = new Dictionary<string, int[]>();
+            results.Add("addr", addr(register, A, B, C));
+            results.Add("addi", addi(register, A, B, C));
+            results.Add("mulr", mulr(register, A, B, C));
+            results.Add("muli", muli(register, A, B, C));
+            results.Add("banr", banr(register, A, B, C));
+            results.Add("bani", bani(register, A, B, C));
+            results.Add("borr", borr(register, A, B, C));
+            results.Add("bori", bori(register, A, B, C));
+            results.Add("setr", setr(register, A, B, C));
+            results.Add("seti", seti(register, A, B, C));
+            results.Add("gtri", gtri(register, A, B, C));
+            results.Add("gtir", gtir(register, A, B, C));
+            results.Add("gtrr", gtrr(register, A, B, C));
+            results.Add("eqir", eqir(register, A, B, C));
+            results.Add("eqri", eqri(register, A, B, C));
+            results.Add("eqrr", eqrr(register, A, B, C));
 
-            List<int[]> results = new List<int[]>();
-            results.Add(addr(register, A, B, C));
-            results.Add(addi(register, A, B, C));
-            results.Add(mulr(register, A, B, C));
-            results.Add(muli(register, A, B, C));
-            results.Add(banr(register, A, B, C));
-            results.Add(bani(register, A, B, C));
-            results.Add(borr(register, A, B, C));
-            results.Add(bori(register, A, B, C));
-            results.Add(setr(register, A, B, C));
-            results.Add(seti(register, A, B, C));
-            results.Add(gtri(register, A, B, C));
-            results.Add(gtir(register, A, B, C));
-            results.Add(gtrr(register, A, B, C));
-            results.Add(eqir(register, A, B, C));
-            results.Add(eqri(register, A, B, C));
-            results.Add(eqrr(register, A, B, C));
-
-
-            var res = results.Where(r => string.Concat(r).Equals(string.Concat(compare))).Count();
-            return res;
+            var res = results.Where(r => string.Concat(r.Value).Equals(string.Concat(compare)));
+            Dictionary<string, int> ret = new Dictionary<string, int>();
+            foreach (var item in res)
+            {
+                ret.Add(item.Key, 1);
+            }
+            return ret;
         }
+    }
+
+    public class Test
+    {
+        public int[] Before { get; set; }
+        public int[] After { get; set; }
+        public int Operation { get; set; }
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
     }
 }
