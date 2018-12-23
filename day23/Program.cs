ca784675f592e52c8859cc9bfa50d847c534b765
Lars Nyström <nystrom.lasse@gmail.com>
2018-12-23 17:48:01 +0100

    Add "solution" to part 2... finds the solution after a while... but it's random

--- day23/Program.cs
+++ day23/Program.cs
@@ -16,7 +16,7 @@ namespace day23
                 .ToList();
 
             Regex rx = new Regex(@"pos=<(-?\d+),(-?\d+),(-?\d+)>, r=(\d+)");
-            
+
             List<NanoBot> nanoBots = new List<NanoBot>();
             foreach (var item in lines)
             {
@@ -31,7 +31,7 @@ namespace day23
                     long pz = long.Parse(groups[3].ToString(), System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture);
                     long r = long.Parse(groups[4].ToString(), System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture);
 
-                    nanoBots.Add(new NanoBot { X = px, Y = py, Z = pz, Radius = r });
+                    nanoBots.Add(new NanoBot(px, py, pz, r));
                 }
             }
 
@@ -40,11 +40,71 @@ namespace day23
 
             Console.WriteLine(nanoBots.Where(n => n.ManhattanDistanceFrom(nanoBot) <= bestRadius).Count());
 
+            Random rand = new Random(DateTime.Now.Millisecond);
+
+            //int minx = (int)nanoBots.Min(n => n.X);
+            //int miny = (int)nanoBots.Min(n => n.Y);
+            //int minz = (int)nanoBots.Min(n => n.Z);
+
+            //int maxx = (int)nanoBots.Max(n => n.X);
+            //int maxy = (int)nanoBots.Max(n => n.Y);
+            //int maxz = (int)nanoBots.Max(n => n.Z);
+
+            int minx = 18140327 - 400000; // (int)nanoBots.Min(n => n.X);
+            int miny = 52767466 - 400000; // (int)nanoBots.Min(n => n.Y);
+            int minz = 58536384 - 400000; // (int)nanoBots.Min(n => n.Z);
+
+            int maxx = 18140327 + 400000; // (int)nanoBots.Min(n => n.X);
+            int maxy = 52767466 + 400000; // (int)nanoBots.Min(n => n.Y);
+            int maxz = 58536384 + 400000; // (int)nanoBots.Min(n => n.Z);
+
+            int nanoBotCount = 0;
+            long shortestDistance = long.MaxValue;
+            while(nanoBotCount < 1000)
+            {
+                long x = rand.Next(minx, maxx);
+                long y = rand.Next(miny, maxy);
+                long z = rand.Next(minz, maxz);
+                var testBot = new NanoBot(x, y, z, 0);
+                var c = CountNanoBotInRange(testBot, nanoBots);
+                if (c >= nanoBotCount && testBot.ManhattanDistance < shortestDistance)
+                {
+                    nanoBotCount = c;
+                    shortestDistance = testBot.ManhattanDistance;
+                    Console.WriteLine($"{x},{y},{z} - {nanoBotCount} - {shortestDistance}");
+
+                }
+            }
+
+            // 133894881 (to high)
+            // 19210606,52359779,59674126 - 878 - 131244511
+            // 18887560,53186405,58963532 - 907 - 131037497
+            // 18140327,52767466,58536384 - 910 - 129444177
+            // 18174334,52961133,58308710 - 910 - 129444177
+        }
+
+        private static int CountNanoBotInRange(NanoBot coord, List<NanoBot> nanoBots)
+        {
+            int nanoBotsInRange = 0;
+
+            foreach (var item in nanoBots)
+            {
+                if (item.ManhattanDistanceFrom(coord) <= item.Radius) nanoBotsInRange++;
+            }
+
+            return nanoBotsInRange;
         }
     }
 
     public class NanoBot
     {
+        public NanoBot(long x, long y, long z, long radius)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            Radius = radius;
+        }
         public long X { get; set; }
         public long Y { get; set; }
         public long Z { get; set; }
